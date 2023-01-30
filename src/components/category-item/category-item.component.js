c65823bd70144c86e7e7d2124264dad58bf35931
Kgl-01 <karthikgowda.mailme@gmail.com>
2023-01-31 01:53:45 +0530

    implemented react-router for navigation between the pages and created navigation bar component

--- src/components/category-item/category-item.component.js
+++ src/components/category-item/category-item.component.js
@@ -4,11 +4,11 @@ const CategoryItem = ({ category }) => {
   const { title, imageUrl } = category;
 
   return (
-    <div
-      className="category-container"
-      style={{ backgroundImage: `url(${imageUrl})` }}
-    >
-      <div className="background-image" />
+    <div className="category-container">
+      <div
+        className="background-image"
+        style={{ backgroundImage: `url(${imageUrl})` }}
+      />
       <div className="category-body-container">
         <h2>{title}</h2>
         <p>SHOP NOW</p>
